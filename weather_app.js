ae492dcd0fcd12826945072502ef3e29e8fa6537
Amo <email-ambrosiusafiandri@gmail.com>
2026-07-26 09:25:10 +0700

    weather_web_app

---
+++ weather_app.js
@@ -0,0 +1,55 @@
+//I want the background to change according to the weather data
+// example: rain (then the background of the rain)
+
+
+
+// we first capture all the html classes that exist.
+const menu = document.querySelector('.menu');
+const calender = document.querySelector('.calender');
+const menu = document.queryElector('.angka');
+const description = document.querySelector('.remarks');
+Const 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Function to display the popup after 1 minute (60000 ms)
+setTimeout(() => {
+  document.getElementById("bdycookies").style.display = "block";
+}, 3,3000000);
+
+// Function to hide the popup when the "Okay" button is clicked
+document.getElementById("oka").addEventListenner("click", function() {
+  document.getElementById("bdycookies").style.display = "none";
+});
